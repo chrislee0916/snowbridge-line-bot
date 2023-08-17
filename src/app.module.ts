651310f177bb00a5237b841526852878dd59e2ca
彥宇 <jos61404@gmail.com>
2023-08-17 15:10:42 +0800

    新增 config 功能

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath:
+        process.env.NODE_ENV === 'prod'
+          ? ['.env.production.local', '.env.production', '.env']
+          : process.env.NODE_ENV === 'dev'
+          ? ['.env.development.local', '.env.development', '.env']
+          : process.env.NODE_ENV === 'test'
+          ? ['.env.test.local', '.env.test', '.env']
+          : '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  Logger.log(configService.get('SERVER_NAME'), ' 啟動服務 ');
+  Logger.log(configService.get('VERSION'), '   版本   ');
+  Logger.log(configService.get('SERVER_PORT'), '   port   ');
+
+  await app.listen(configService.get('SERVER_PORT'));
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
